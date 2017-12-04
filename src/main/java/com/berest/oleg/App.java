d90e2157e79aa7aac7ff95dc0701bb5357fc2932
olbe0615 <oleg.berest@NetCracker.com>
2017-12-04 20:27:49 +0200

    add new method

--- src/main/java/com/berest/oleg/App.java
+++ src/main/java/com/berest/oleg/App.java
@@ -8,4 +8,8 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
+
+    public static void share(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
